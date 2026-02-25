ca93ed0af2c94016388e551d0c05b35878c29c69
Luis Puc <contacto@luispsarmiento.com>
2026-02-24 21:48:13 -0600

    Changed: implement initial Azure Communication Services integration

---
+++ azure/communication-services/SMSChannel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Azure;
+using Azure.Communication;
+using Azure.Communication.Sms;
+
+string connectionString = Environment.GetEnvironmentVariable("COMMUNICATION_SERVICES_CONNECTION_STRING");
+string toPhoneNumber = Environment.GetEnvironmentVariable("TO_PHONE_NUMBER");
+string partnerApiKey = Environment.GetEnvironmentVariable("PARTNER_API_KEY");
+
+SmsClient smsClient = new SmsClient(connectionString);
+// Send SMS with Messaging Connect
+SmsSendResult sendResult = smsClient.Send(
+    from: "ATSMSTest",
+    to: toPhoneNumber,
+    message: "Hello World via SMS",
+    options: new SmsSendOptions(enableDeliveryReport: true)
+    {
+        Tag = "OTP",
+        MessagingConnect = new MessagingConnectOptions(partnerApiKey, "infobip")
+    });
+
+Console.WriteLine($"Sms id: {sendResult.MessageId}");
